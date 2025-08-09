92e04780b61a541f7f251d9a76dc79fb12a059c4
theokiwi <theodinizv@gmail.com>
2025-08-09 20:14:57 -0300

    tentativa 19

--- knexfile.js
+++ knexfile.js
@@ -1,58 +1,44 @@
+// Update with your config settings.
+
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+
 require('dotenv').config();
-const path = require('path');
 
 module.exports = {
+
   development: {
     client: 'pg',
     connection: {
-      host: process.env.POSTGRES_HOST || 'localhost',
-      database: process.env.POSTGRES_DB,
+      host: '127.0.0.1',
+      port: 5432,
       user: process.env.POSTGRES_USER,
       password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
     },
-    pool: { min: 2, max: 10 },
     migrations: {
-      directory: path.join(__dirname, 'db', 'migrations'),
-      tableName: 'knex_migrations',
-    },
+        directory: './db/migrations',
+      },
     seeds: {
-      directory: path.join(__dirname, 'db', 'seeds'),
-    },
+        directory: './db/seeds',
+      },
   },
-
-  staging: {
+  ci: {
     client: 'pg',
     connection: {
-      host: process.env.POSTGRES_HOST || 'localhost',
-      database: process.env.POSTGRES_DB,
+      host: 'postgres', 
+      port: 5432,
       user: process.env.POSTGRES_USER,
       password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
     },
-    pool: { min: 2, max: 10 },
     migrations: {
-      directory: path.join(__dirname, 'db', 'migrations'),
-      tableName: 'knex_migrations',
+      directory: './db/migrations',
     },
     seeds: {
-      directory: path.join(__dirname, 'db', 'seeds'),
+      directory: './db/seeds',
     },
-  },
+  }
 
-  production: {
-    client: 'pg',
-    connection: {
-      host: process.env.POSTGRES_HOST || 'localhost',
-      database: process.env.POSTGRES_DB,
-      user: process.env.POSTGRES_USER,
-      password: process.env.POSTGRES_PASSWORD,
-    },
-    pool: { min: 2, max: 10 },
-    migrations: {
-      directory: path.join(__dirname, 'db', 'migrations'),
-      tableName: 'knex_migrations',
-    },
-    seeds: {
-      directory: path.join(__dirname, 'db', 'seeds'),
-    },
-  },
 };
