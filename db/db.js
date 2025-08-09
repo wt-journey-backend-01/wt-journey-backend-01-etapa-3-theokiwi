97521acf88fcbcb00becf951f43cedae1b3432b3
theokiwi <theodinizv@gmail.com>
2025-08-09 19:51:20 -0300

    tentativa 14

--- db/db.js
+++ db/db.js
@@ -1,7 +1,6 @@
-const environment = process.env.NODE_ENV || 'development';
-const config = require('../knexfile')[environment];
 const knex = require('knex');
+const config = require('../knexfile');
 
-const db = knex(config);
+const db = knex(config.development);
 
 module.exports = db;
